42f7cb83ee1af779ec44ff5fdde522524e09175c
Andreas <93956486+pujo04@users.noreply.github.com>
2024-04-13 16:27:48 +0700

    Update fashion.js

--- fashion.js
+++ fashion.js
@@ -1,3 +1,21 @@
+/* Hamburger Menu */
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika Humberger Menu Diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik Diluar Sidebar Untuk Menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
 /* Slidebar */ 
 let slideIndex = 1;
 showSlides(slideIndex);
